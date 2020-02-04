4b1394db5c929be5b414a43dae689437e3bb5f27
Velebnik Serhii <serhii.velebnik@gmail.com>
2020-02-04 19:28:32 +0200

    add cart page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Store';
-
-  backpacks=[
-{id:1,name:"Ospey1",label:"Men's backpack",price:120,img:"assets/img/TE_denali_black_red_17-200x200.jpg"},
-{id:2,name:"Ospey2",label:"Men's backpack",price:140,img:"assets/img/TE_denali_black_red_17-200x200.jpg"},
-{id:3,name:"Ospey3",label:"Men's backpack",price:620,img:"assets/img/TE_denali_black_red_17-200x200.jpg"},
-{id:4,name:"Ospey4",label:"Men's backpack",price:420,img:"assets/img/TE_denali_black_red_17-200x200.jpg"},
-
-  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { BackpacksComponent } from './backpacks/backpacks.component';
+import { CartComponent } from './cart/cart.component';
+
+
+const appRoutes: Routes = [
+  { path: 'backpacks', component: BackpacksComponent },
+  { path: 'cart',      component: CartComponent },
+  { path: '',
+    redirectTo: '/backpacks',
+    pathMatch: 'full'
+  },
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    BackpacksComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,6 +34,9 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent],
