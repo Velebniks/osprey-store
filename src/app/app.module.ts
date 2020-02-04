1895692942ef2faa4dcc59f863ec05885653f33c
Velebnik Serhii <serhii.velebnik@gmail.com>
2020-02-04 18:36:30 +0200

    add cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import{MatInputModule}from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatInputModule,
+    MatCardModule,
+  
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
+
+
 export class AppModule { }
